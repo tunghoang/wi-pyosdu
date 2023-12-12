c47d5dce579f2cb0db44732c564d24a63a7bb043
Ubuntu <tung.hoang@gmail.com>
2023-12-12 03:06:57 +0000

    update

--- client/Entitlement.py
+++ client/Entitlement.py
@@ -9,6 +9,21 @@ def entitlement_info():
   print(resp.content.decode('utf-8'))
 
 def entitlement_list_groups():
-  resp = httpPostJson(f'{__ENTITLEMENT_BASE_URL}/groups', headers=auth_admin_headers())
-  print(resp.content.decode('utf-8'))
+  headers = auth_admin_headers()
+  resp = httpGet(f'{__ENTITLEMENT_BASE_URL}/groups', headers=headers)
+  return resp.json()
 
+def entitlement_list_members(group):
+  resp = httpGet(f'{__ENTITLEMENT_BASE_URL}/groups/{group}/members', headers=auth_admin_headers())
+  return resp.json()
+  
+def entitlement_add_to_group(user, group, owner=False):
+  resp = httpPostJson(f'{__ENTITLEMENT_BASE_URL}/groups/{group}/members', headers=auth_admin_headers(), json = {
+    'email': user,
+    'role': 'OWNER' if owner else 'MEMBER'
+  })
+  return resp.json()
+
+def entitlement_remove_from_group(user, group):
+  resp = httpDelete(f'{__ENTITLEMENT_BASE_URL}/groups/{group}/members/{user}', headers=auth_admin_headers())
+  print(resp.content.decode('utf-8'))
--- entitlement.py
+++ entitlement.py
@@ -11,9 +11,17 @@ parser.add_argument('--add2group', action='store_true')
 parser.add_argument('--user')
 parser.add_argument('--group')
 parser.add_argument('--list_groups', action='store_true')
+parser.add_argument('--list_members', action='store_true')
+parser.add_argument('--removefromgroup', action="store_true")
 args = parser.parse_args()
 
 if args.list_groups:
   print(json.dumps(entitlement_list_groups()))
 elif args.info:
   entitlement_info()
+elif args.list_members and args.group:
+  print(json.dumps(entitlement_list_members(args.group)))
+elif args.add2group and args.user and args.group:
+  print(json.dumps(entitlement_add_to_group(args.user, args.group)))
+elif args.removefromgroup and args.user and args.group:
+  entitlement_remove_from_group(args.user, args.group)
