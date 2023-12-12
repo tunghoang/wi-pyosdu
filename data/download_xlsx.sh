7d3e332644982db12f2d452fad819eae1c9da05b
Tung HOANG <tung.hoang@gmail.com>
2023-12-12 10:17:23 +0700

    add download_slsx.sh

---
+++ data/download_xlsx.sh
@@ -0,0 +1,2 @@
+wget -O input.xlsx https://docs.google.com/spreadsheets/d/1WaL_iwuQgoDfBBOT8ESFlEcCKBC3HdI2UPsVnsOFt4Q/export?format=xlsx
+wget -O mappings.xlsx https://docs.google.com/spreadsheets/d/1EuBN8SLZUHj9jETWqpAs2enJGH67zPbtrf2V4zG3924/export?format=xlsx
