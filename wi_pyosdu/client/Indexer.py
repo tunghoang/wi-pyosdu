9281ef8e0e2874f6136b7a84cda6e9f26c42d476
Tung HOANG <tung.hoang@gmail.com>
2023-12-16 00:16:32 +0700

    update

---
+++ wi_pyosdu/client/Indexer.py
@@ -0,0 +1,18 @@
+from .Token import access_token, auth_headers, auth_admin_headers
+from ..config import *
+from .Http import *
+import json
+
+__INDEXER_BASE_URL = f'{OSDU_BASE}/api/indexer/v2'
+def indexer_info():
+  resp = httpGet(f'{__INDEXER_BASE_URL}/info', headers=auth_headers())
+  print(resp.content.decode('utf-8'))
+
+def indexer_reindex_kind(kind, cursor):
+  #headers = auth_admin_headers()
+  headers = auth_headers()
+  resp = httpPostJson(f'{__INDEXER_BASE_URL}/reindex', json={
+    'kind': kind,
+    'cursor': cursor
+  },headers=headers)
+  print(resp.content.decode('utf-8'))
---
+++ wi_pyosdu/indexer.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import json
+from os import environ
+from .config import *
+from .client.Indexer import *
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--info', action="store_true")
+parser.add_argument('--reindex', action="store_true")
+parser.add_argument('--kind')
+parser.add_argument('--cursor')
+args = parser.parse_args()
+
+if args.info:
+  indexer_info()
+elif args.reindex and args.kind and args.cursor:
+  indexer_reindex_kind(args.kind, args.cursor)
