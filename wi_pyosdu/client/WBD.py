e265f04e08770e23a063253afddd5eb3440a80f8
Tung HOANG <tung.hoang@gmail.com>
2023-12-18 13:03:53 +0700

    update

--- wi_pyosdu/client/WBD.py
+++ wi_pyosdu/client/WBD.py
@@ -1,6 +1,8 @@
 from .Token import access_token, auth_headers
 from ..config import *
 from .Http import httpGet, httpPutJson, httpPostJson, httpDelete
+from .Search import *
+from .File import *
 from ..models.Record import Record
 import json, io
 from pandas import read_parquet
@@ -19,5 +21,14 @@ def wbd_welllog_data(record_id):
   return read_parquet(io.BytesIO(resp.content))
 
 def wbd_delete_welllog(welllog_id):
+  result = search_query('osdu:wks:work-product-component--WellLog:*', f'id:"{welllog_id}"', returnedFields=['id', 'data.Datasets'])
+  if result['totalCount'] == 0:
+    print(f"No welllog found {welllog_id}")
+    return
+  print(result) 
+  datasets = result['results'][0]['data']['Datasets']
+  for ds in datasets:
+    file_delete_record(ds[:-1])
+
   resp = httpDelete(f'{__WBD_BASE_URL}/ddms/v3/welllogs/{welllog_id}', headers=auth_headers())
   print(resp.content)
